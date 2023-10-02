3ec241a5bd4dcfa0509eab3cd708f13072b396e0
Per Christian Moen <per.christian.moen@nav.no>
2023-10-02 17:26:05 +0200

    Use oauth2 defaults

--- src/main/kotlin/no/nav/pensjon/gateway/pensjonazureadappgateway/PensjonAzureadAppGatewayApplication.kt
+++ src/main/kotlin/no/nav/pensjon/gateway/pensjonazureadappgateway/PensjonAzureadAppGatewayApplication.kt
@@ -4,24 +4,29 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.security.reactive.ReactiveUserDetailsServiceAutoConfiguration
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
+import org.springframework.security.config.Customizer.withDefaults
 import org.springframework.security.config.web.server.ServerHttpSecurity
+import org.springframework.security.config.web.server.ServerHttpSecurity.AuthorizeExchangeSpec
 import org.springframework.security.web.server.SecurityWebFilterChain
 
 
 @SpringBootApplication(exclude = [ReactiveUserDetailsServiceAutoConfiguration::class])
+
 class PensjonAzureadAppGatewayApplication {
-	@Bean
-	fun springSecurityFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
-		http
-			.authorizeExchange { authorize: ServerHttpSecurity.AuthorizeExchangeSpec ->
-				authorize
-					.pathMatchers("/actuator/health/**").permitAll()
-					.pathMatchers("/actuator/prometheus/**").permitAll()
-					.anyExchange().authenticated()
-			}
-		return http.build()
-	}
+    @Bean
+    fun springSecurityFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
+        http.authorizeExchange { authorize: AuthorizeExchangeSpec ->
+            authorize
+                .pathMatchers("/actuator/health/**").permitAll()
+                .pathMatchers("/actuator/prometheus/**").permitAll()
+                .anyExchange().authenticated()
+        }
+        http.oauth2Login(withDefaults())
+        http.oauth2Client(withDefaults())
+        return http.build()
+    }
 }
+
 fun main(args: Array<String>) {
-	runApplication<PensjonAzureadAppGatewayApplication>(*args)
+    runApplication<PensjonAzureadAppGatewayApplication>(*args)
 }
