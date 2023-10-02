5f71357afef495c515af4aabee928422d22428e9
Per Christian Moen <per.christian.moen@nav.no>
2023-10-02 08:17:00 +0200

    Applikasjon generert av Spring Initializr

---
+++ src/test/kotlin/no/nav/pensjon/gateway/pensjonazureadappgateway/PensjonAzureadAppGatewayApplicationTests.kt
@@ -0,0 +1,13 @@
+package no.nav.pensjon.gateway.pensjonazureadappgateway
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PensjonAzureadAppGatewayApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
