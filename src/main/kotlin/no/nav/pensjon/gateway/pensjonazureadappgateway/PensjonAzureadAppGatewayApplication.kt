2f70a5b955d6e3aa067be8b3465e50d5ed6c9cd3
Per Christian Moen <per.christian.moen@nav.no>
2023-11-04 16:08:56 +0100

    Log requests

--- src/main/kotlin/no/nav/pensjon/gateway/pensjonazureadappgateway/PensjonAzureadAppGatewayApplication.kt
+++ src/main/kotlin/no/nav/pensjon/gateway/pensjonazureadappgateway/PensjonAzureadAppGatewayApplication.kt
@@ -1,9 +1,7 @@
 package no.nav.pensjon.gateway.pensjonazureadappgateway
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.security.reactive.ReactiveUserDetailsServiceAutoConfiguration
 import org.springframework.boot.runApplication
-
 
 @SpringBootApplication
 class PensjonAzureadAppGatewayApplication
